cb6d9927360f6eb827b2480e982930f462cd1f07
Fedi Nabli <fedinabli@gmail.com>
2022-12-29 21:21:11 +0100

    Conntected client to server

--- client/script.js
+++ client/script.js
@@ -83,6 +83,33 @@ const handleSubmit = async (e) => {
 
   // messageDiv.innerHTML = "..."
   loader(messageDiv)
+
+  //fetch response from server -> bot's response
+  const response = await fetch('http://localhost:5000', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      prompt: data.get('prompt')
+    })
+  })
+
+  clearInterval(loadInterval)
+  messageDiv.innerHTML = ''
+
+  if (response.ok) {
+    const data = await response.json()
+    const parseData = data.bot.trim()
+
+    typeText(messageDiv, parseData)
+  } else {
+    const err = await response.text()
+
+    messageDiv.innerHTML = "Something went wrong"
+
+    alert(err)
+  }
 }
 
 form.addEventListener('submit', handleSubmit)
