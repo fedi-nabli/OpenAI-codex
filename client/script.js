553486a5acf7df2ca111db2f95c2f9d5f4ffd346
Fedi Nabli <fedinabli@gmail.com>
2022-12-29 21:51:21 +0100

    Updated backend link

--- client/script.js
+++ client/script.js
@@ -85,7 +85,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv)
 
   //fetch response from server -> bot's response
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://codex-k3jf.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
